709262c1a4703a6e0471eb34e1e86083a365ed59
alyssalbailey <alyssabailey@me.com>
2021-06-16 10:03:00 -0400

    Updating display functions.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Global Variables 
 let employees = [];
 const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
-email, location, phone, dob &noinfo &nat=US`
+email, location, phone, dob &noinfo &nat=US`;
 const gridContainer = document.querySelector(".grid-container");
 const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal-content");
@@ -14,7 +14,7 @@ fetch(urlAPI)
 .then(displayEmployees)
 .catch( err => console.log(err))
 
-displayEmployees(employeeData) {
+function displayEmployees(employeeData){
     employees = employeeData;
     let employeeHTML = ""; // store the employee HTML as we create it
 
@@ -22,13 +22,13 @@ displayEmployees(employeeData) {
     employees.forEach(( employee, index) => {
         let name = employee.name;
         let email = employee.email;
-        let city = employee.city;
+        let city = employee.location.city;
         let picture = employee.picture;
 
         employeeHTML += `
-            <div class="card" data-index=${index}">
-                <img class="avatar" src=${picture.large}"/>
-                <div class="text-container"
+            <div class="card" data-index="${index}">
+                <img class="avatar" src="${picture.large}"/>
+                <div class="text-container">
                     <h2 class="name">${name.first} ${name.last}</h2>
                     <p class="email">${email}</p>
                     <p class="address">${city}</p> 
@@ -37,13 +37,19 @@ displayEmployees(employeeData) {
         `
     });
         gridContainer.innerHTML = employeeHTML;
-
 }
 
-displayModal(index){
+
+function displayModal(index){
     
     // use object destructuring
-    let { name, dob, phone, email, location: {city, street, state, postcode}, picture } = employees[index];
+    let {
+        name, 
+        dob, 
+        phone, 
+        email, 
+        location: {city, street, state, postcode}, picture } 
+        = employees[index];
 
     let date = new Date (dob.date);
 
@@ -63,7 +69,7 @@ displayModal(index){
 
     overlay.classList.remove("hidden");
     modalContainer.innerHTML = modalHTML;
-    }
+    };
 
     gridContainer.addEventListener('click', e => {
         // make sure the click is not on the gridContainer itself
