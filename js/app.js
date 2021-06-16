980d4a28a2d93e5f2e6027b2e7e09fa2e3080c69
alyssalbailey <alyssabailey@me.com>
2021-06-16 11:52:11 -0400

    Adding additional styles to the modal + grid layout.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 // Global Variables 
-let employees = [];
+
 const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
 email, location, phone, dob &noinfo &nat=US`;
 const gridContainer = document.querySelector(".grid-container");
 const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");
+
+let employees = [];
 
 //Fetch data from the API
 fetch(urlAPI)
@@ -61,7 +63,7 @@ function displayModal(index){
         <p class="address">${city}</p>
         <hr />
         <p>${phone}</p>
-        <p class="address">${street}, ${state} ${postcode}</p>
+        <p class="address">${street.number} ${street.name}, ${state} ${postcode}</p>
         <p>Birthday:
     ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
     </div>
@@ -74,10 +76,10 @@ function displayModal(index){
     gridContainer.addEventListener('click', e => {
         // make sure the click is not on the gridContainer itself
         if (e.target !== gridContainer) {
-        // select the card element based on its proximity to actual element
-        clicked
+        // select the card element based on its proximity to actual element clicked
         const card = e.target.closest(".card");
         const index = card.getAttribute('data-index');
+
         displayModal(index);
         }
     });
